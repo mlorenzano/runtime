e3422c60904fa90ffc45346cf07ebcdbaa17af3a
Federico Guerinoni <guerinoni@micro-systems.it>
2019-03-19 18:32:08 +0100

    Added action to load file.

--- runtime/src/app/mainwindow.cpp
+++ runtime/src/app/mainwindow.cpp
@@ -1,14 +1,26 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QFileDialog>
+#include <QStandardPaths>
+#include <QDebug>
+
 MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
+                                          QMainWindow(parent),
+                                          ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
 
     connect(this->ui->actionExit, &QAction::triggered,
             this, [&](){ QApplication::quit(); });
+
+    connect(this->ui->actionOpen, &QAction::triggered,
+            this, [&](){
+                auto l = QStandardPaths::locate(QStandardPaths::DesktopLocation, "");
+                auto f = QFileDialog::getOpenFileName(this, "Open File", l,
+                                                      "Runtime (*.xlsx *.xls *.csv)");
+                qDebug() << f;
+            });
 }
 
 MainWindow::~MainWindow()
